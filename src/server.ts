3769bacd83a300ece751b4893887462e5148f95b
theBashShell <edohbruno@gmail.com>
2019-10-28 17:35:57 +0000

    helmet added

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
+import helmet from 'helmet';
 import homeRouter from './routes/home';
 import {PORT} from './utils/serversetup';
 import express, {Application, Request, Response} from 'express';
 
 const app: Application = express();
 
+app.use(helmet());
 app.use(homeRouter);
 
 app.listen(PORT, () => console.log(`App on http://0.0.0.0:${PORT}`));
