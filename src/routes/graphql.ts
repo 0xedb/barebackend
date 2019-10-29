772be1a9793d885e6679285846d9ab56772d0745
theBashShell <edohbruno@gmail.com>
2019-10-29 00:55:02 +0000

    graphql endpoint working

--- src/routes/graphql.ts
+++ src/routes/graphql.ts
@@ -5,12 +5,12 @@ import crypto from 'crypto';
 
 const schema = buildSchema(`
   type Query {
-    myEmail(email: String!): String!
+    code(email: String!): String!
   }
 `);
 
 var rootValue = {
-  myEmail: ({email}: {email: string}): string => {
+  code: ({email}: {email: string}): string => {
     return crypto.randomBytes(10).toString('hex');
   },
 };
@@ -22,7 +22,7 @@ router.use(
   graphqlHTTP({
     schema,
     rootValue,
-    graphiql: true,
+    graphiql: process.env.BARE_ENV === 'production' ? false : true,
   })
 );
 export default router;
--- src/utils/headmiddleware.ts
+++ src/utils/headmiddleware.ts
@@ -1,14 +1,13 @@
 import {Request, Response, NextFunction} from 'express';
 
 export default (req: Request, res: Response, next: NextFunction): void => {
-  if (process.env.BARE_ENV === 'production')
-    res.set({
-      'Content-Security-Policy': 'default-src https frame-ancestors none',
-      'Strict-Transport-Security':
-        ' max-age=63072000; includeSubDomains; preload',
-      'X-Frame-Options': 'DENY',
-      'X-Content-Type-Options': 'nosniff',
-      'X-XSS-Protection': '1; mode=block',
-    });
+  res.set({
+    'Content-Security-Policy': 'default-src https frame-ancestors none',
+    'Strict-Transport-Security':
+      ' max-age=63072000; includeSubDomains; preload',
+    'X-Frame-Options': 'DENY',
+    'X-Content-Type-Options': 'nosniff',
+    'X-XSS-Protection': '1; mode=block',
+  });
   next();
 };
