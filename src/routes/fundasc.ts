584d763fbb44cde3763572f60b8122e64884a499
theBashShell <edohbruno@gmail.com>
2019-10-31 21:44:02 +0000

    email sender works

--- src/routes/fundasc.ts
+++ src/routes/fundasc.ts
@@ -1,6 +1,7 @@
 import crypto from 'crypto';
 import cors from 'cors';
 import {Router, Request, Response} from 'express';
+import sendMail from '../utils/mailer';
 import {buildSchema} from 'graphql';
 import graphqlHTTP from 'express-graphql';
 
@@ -13,28 +14,44 @@ const schema = buildSchema(`
   }
 `);
 
-let rootValue = {
-  code: ({email}: {email: string}): string => {
-    return crypto.randomBytes(10).toString('hex');
+const transportOptions = {
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true, // true for 465, false for other ports
+  auth: {
+    user: process.env.FUNDUS_USER as string,
+    pass: process.env.FUNDUS_PASS as string,
   },
 };
 
-if (process.env.BARE_ENV !== 'production') {
-  router.use(
-    '/graphql',
-    graphqlHTTP({
-      schema,
-      rootValue,
-      graphiql: true,
-    })
-  );
-} else {
-  const graphql = graphqlHTTP({
-    schema,
-    rootValue,
-    graphiql: false,
-  });
+const mailOptions = {
+  from: '"FundUs 👻" <fundus.flask@gmail.com>', // sender address
+  to: 'edohbruno@gmail.com', // list of receivers
+  subject: 'Login Code ✔', // Subject line
+  text: '', // plain text body
+  html: '', // html body
+};
 
+let rootValue = {
+  code: ({email}: {email: string}): string => {
+    const bytes = crypto.randomBytes(10).toString('hex');
+    mailOptions.text = `Login code is: ${bytes}`;
+    mailOptions.html = `Login code is: <b><pre>${bytes}</pre></b>`;
+    sendMail(transportOptions, mailOptions);
+    return bytes;
+  },
+};
+
+const env = process.env.BARE_ENV !== 'production';
+const graphql = graphqlHTTP({
+  schema,
+  rootValue,
+  graphiql: env,
+});
+
+if (env) {
+  router.use('/graphql', graphql);
+} else {
   router.post('/graphql', graphql);
 }
 
--- src/utils/mailer.ts
+++ src/utils/mailer.ts
@@ -1,68 +1,27 @@
-import nodeMailer from 'nodemailer';   
+import nodeMailer from 'nodemailer';
 
+export interface MailerOptions {  
+  from: string;
+  to: string;
+  subject: string;
+  html: string;
+  text: string;
+}
 
- 
+export interface TransporterOptions {
+  host: string;
+  port: number;
+  secure: boolean;
+  auth: {
+    user: string;
+    pass: string;
+  };
+}
 
-// interface MailerOptions {
-//   email: string;
-//   code: string;
-//   from: string;
-//   to: string;
-//   subject: string;
-//   html: string;
-//   text: string;
-// }
-
-// interface TransporterOptions {
-//   host: string;
-//   port: number;
-//   secure: boolean;
-//   user: string;
-//   pass: string;
-// }
-
-// async function sendEmail({
-//   email,
-//   code,
-//   from,
-//   to,
-//   subject,
-//   text,
-//   html,
-// }: MailerOptions) {
-//   let transporter = ({
-//     host,
-//     port = 465,
-//     secure = true,
-//     user,
-//     pass,
-//   }: TransporterOptions): Mail =>
-//     nodeMailer.createTransport({
-//       host,
-//       port,
-//       secure,
-//       auth: {
-//         user,
-//         pass,
-//       },
-//     });
-
-//   let mailOptions = {
-//     from,
-//     to,
-//     subject,
-//     text,
-//     html,
-//   };
-  
-// transporter.sendMail(mailOptions, (err, info) => {
-//     if (err) return console.log(err);
-//     console.log('Message sent: %s', info.messageId);
-//   });
- 
- 
-// from: '"FundUs 👻" <fundus.flask@gmail.com>', // sender address
-//     to: 'edohbruno@gmail.com', // list of receivers
-//     subject: 'Login Code ✔', // Subject line
-//     text: `Login code is: ${code}`, // plain text body
-//     html: `Login code is: <b><pre>${code}</pre></b>`, // html body
+export default async (transport: TransporterOptions, mail: MailerOptions) => {
+  nodeMailer
+    .createTransport(transport)
+    .sendMail(mail)
+    .then(a => console.log('sent'))
+    .catch(error => console.log(error));
+};
